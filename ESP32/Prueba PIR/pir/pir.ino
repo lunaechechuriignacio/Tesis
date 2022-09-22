fecbbc00c47d02b715f8a43871740d82d17cf4d2
Ignacio Luna Echechuri <inle_one@hotmail.com>
2022-09-22 15:43:12 -0300

    pruebas pir y relay
    Pruebas

---
+++ ESP32/Prueba PIR/pir/pir.ino
@@ -0,0 +1,37 @@
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-motion-sensor
+ */
+#define LED 2
+const int PIN_TO_SENSOR = 26; // GIOP19 pin connected to OUTPUT pin of sensor
+int pinStateCurrent   = LOW;  // current state of pin
+int pinStatePrevious  = LOW;  // previous state of pin
+
+void setup() {
+  Serial.begin(11500);            // initialize serial
+  pinMode(LED,OUTPUT);
+  pinMode(PIN_TO_SENSOR, INPUT); // set ESP32 pin to input mode to read value from OUTPUT pin of sensor
+}
+
+void loop() {
+  pinStatePrevious = pinStateCurrent; // store old state
+  pinStateCurrent = digitalRead(PIN_TO_SENSOR);   // read new state
+
+  if (pinStatePrevious == LOW && pinStateCurrent == HIGH) {   // pin state change: LOW -> HIGH
+    
+    Serial.println("Motion detected!");
+     delay(50);
+    digitalWrite(LED,LOW);
+    // TODO: turn on alarm, light or activate a device ... here
+  }
+  else
+  if (pinStatePrevious == HIGH && pinStateCurrent == LOW) {   // pin state change: HIGH -> LOW
+    Serial.println("Motion stopped!");
+    delay(50);
+     digitalWrite(LED,HIGH);
+    // TODO: turn off alarm, light or deactivate a device ... here
+  }
+}
---
+++ ESP32/Prueba Relay/relay/relay.ino
@@ -0,0 +1,48 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-relay-module-ac-web-server/
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+const int relay_0 =14 ;
+const int relay_1 = 27;
+const int relay_2 = 26;
+const int relay_3 = 25;
+const int relay_4 = 33;
+const int relay_5 = 32;
+const int relay_6 = 16;
+const int relay_7 = 17;
+
+void setup() {
+  pinMode(relay_0, OUTPUT);
+  pinMode(relay_1, OUTPUT);
+  pinMode(relay_2, OUTPUT);
+  pinMode(relay_3, OUTPUT);
+pinMode(relay_4, OUTPUT);
+pinMode(relay_5, OUTPUT);
+pinMode(relay_6, OUTPUT);
+pinMode(relay_7, OUTPUT);
+ 
+}
+
+void loop() {
+  digitalWrite(relay_0, HIGH);
+  digitalWrite(relay_1, HIGH);
+  digitalWrite(relay_2, HIGH);
+  digitalWrite(relay_3, HIGH);
+  digitalWrite(relay_4, HIGH);
+  digitalWrite(relay_5, HIGH);
+  digitalWrite(relay_6, HIGH);
+ digitalWrite(relay_7, HIGH);
+  delay(5000);
+  digitalWrite(relay_0, LOW);
+  digitalWrite(relay_1, LOW);
+  digitalWrite(relay_2, LOW);
+  digitalWrite(relay_3, LOW);
+  digitalWrite(relay_4, LOW);
+  digitalWrite(relay_5, LOW);
+  digitalWrite(relay_6, LOW);
+ digitalWrite(relay_7, LOW);
+  delay(5000);
+}
