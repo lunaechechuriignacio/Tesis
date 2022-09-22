bec1bef91e32ce3e1330affeab35941fee9d4e3e
Ignacio Luna Echechuri <inle_one@hotmail.com>
2022-09-22 16:24:43 -0300

    Update pir.ino

--- ESP32/Prueba PIR/pir/pir.ino
+++ ESP32/Prueba PIR/pir/pir.ino
@@ -11,15 +11,18 @@ int pinStateCurrent   = LOW;  // current state of pin
 int pinStatePrevious  = LOW;  // previous state of pin
 
 void setup() {
-  Serial.begin(11500);            // initialize serial
+  Serial.begin(115200);            // initialize serial
   pinMode(LED,OUTPUT);
   pinMode(PIN_TO_SENSOR, INPUT); // set ESP32 pin to input mode to read value from OUTPUT pin of sensor
 }
 
 void loop() {
+   
   pinStatePrevious = pinStateCurrent; // store old state
   pinStateCurrent = digitalRead(PIN_TO_SENSOR);   // read new state
-
+  Serial.println(pinStateCurrent);
+    Serial.println(pinStatePrevious);
+    delay (150);
   if (pinStatePrevious == LOW && pinStateCurrent == HIGH) {   // pin state change: LOW -> HIGH
     
     Serial.println("Motion detected!");
