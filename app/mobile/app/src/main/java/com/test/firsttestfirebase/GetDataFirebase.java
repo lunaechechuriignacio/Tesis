87c6b8d78ce46262e3d7e77c90efef4465d7aab2
lunaechechuriignacio <inle_one@hotmail.com>
2023-01-11 22:23:15 -0300

    Se hizo una prueba con un nodo con tipo de dato int

---
+++ app/mobile/app/src/main/java/com/test/firsttestfirebase/GetDataFirebase.java
@@ -0,0 +1,18 @@
+package com.test.firsttestfirebase;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class GetDataFirebase {
+    FirebaseDatabase database;
+    String pathNodo;
+
+    void getDataFirebase(FirebaseDatabase database){
+
+    }
+
+
+
+
+
+
+}
--- app/mobile/app/src/main/java/com/test/firsttestfirebase/MainActivity.java
+++ app/mobile/app/src/main/java/com/test/firsttestfirebase/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,24 +14,33 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String  path="Test";
-    private static final String  pathTestString="TestString";
+   // private static final String  path="Test";
+   // private static final String  pathTestString="TestString";
+    private static final String  path="pir_sensor";
+    private static final String  pathTestString="automatic";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView tv= findViewById(R.id.textTest);
+        final TextView dataStringTextView= findViewById(R.id.textTest);
+        final TextView dataIntTextView=findViewById(R.id.dataInt);
         FirebaseDatabase database=FirebaseDatabase.getInstance();
         final DatabaseReference reference= database.getReference(path).child(pathTestString);
         reference.addValueEventListener(new ValueEventListener() {
              @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                tv.setText(snapshot.getValue(String.class));
+                String value=snapshot.getValue(Integer.class).toString();
+                 dataIntTextView.setText(value);
+
+                // value=snapshot.getValue(String.class);
+               //  dataStringTextView.setText(value);
+
+
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
             }
         });
 
