1cce527718ae3909cdd36f6b9a785dde9d9f8708
GonzaloNahuelDP <gonzalo.nahuel.dipierro@gmail.com>
2023-03-25 22:02:21 -0300

    UPDATE: desacople entre del Adapter y el ViewHolder de los Relays

--- app/mobile/app/src/main/java/com/test/firsttestfirebase/view/adapter/RelayRecyclerViewAdapter.java
+++ app/mobile/app/src/main/java/com/test/firsttestfirebase/view/adapter/RelayRecyclerViewAdapter.java
@@ -3,54 +3,18 @@ package com.test.firsttestfirebase.view.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Switch;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.test.firsttestfirebase.R;
-import com.test.firsttestfirebase.mapper.DataMapper;
 import com.test.firsttestfirebase.model.Relay;
-import com.test.firsttestfirebase.service.FirebaseService;
+import com.test.firsttestfirebase.view.viewHolder.RelayViewHolder;
 import java.util.List;
 
-public class RelayRecyclerViewAdapter extends RecyclerView.Adapter<RelayRecyclerViewAdapter.RelayViewHolder> {
+public class RelayRecyclerViewAdapter extends RecyclerView.Adapter<RelayViewHolder> {
     private List<Relay> relayList;
 
     public RelayRecyclerViewAdapter(List<Relay> relayList) {
         this.relayList = relayList;
-    }
-
-    public class RelayViewHolder extends RecyclerView.ViewHolder {
-        private TextView tvRelay;
-        private Switch swRelayAutomatic;
-        private Switch swRelayLight;
-
-        public RelayViewHolder(@NonNull View itemView) {
-            super(itemView);
-            this.tvRelay = itemView.findViewById(R.id.tv_relay);
-            this.swRelayAutomatic = itemView.findViewById(R.id.sw_relay_automatic);
-            this.swRelayLight = itemView.findViewById(R.id.sw_relay_light);
-        }
-
-        public void bind(Relay relay) {
-            this.tvRelay.setText(DataMapper.toCleanComponentAlias(relay.getAlias()));
-
-            this.swRelayAutomatic.setChecked(relay.isAutomatic());
-            this.setEnabledSwRelayLight();
-            this.swRelayAutomatic.setOnClickListener(view -> {
-                FirebaseService.getInstance().setRelayPropertyValue(relay.getNumber(), "automatic", swRelayAutomatic.isChecked());
-                this.setEnabledSwRelayLight();
-            });
-
-            this.swRelayLight.setChecked(DataMapper.toBoolean(relay.getRelayStatus()));
-            this.swRelayLight.setOnClickListener(view -> {
-                FirebaseService.getInstance().setRelayPropertyValue(relay.getNumber(), "status", DataMapper.toInteger(swRelayLight.isChecked()));
-            });
-        }
-
-        private void setEnabledSwRelayLight() {
-            this.swRelayLight.setEnabled(!this.swRelayAutomatic.isChecked());
-        }
     }
 
     @NonNull
---
+++ app/mobile/app/src/main/java/com/test/firsttestfirebase/view/viewHolder/RelayViewHolder.java
@@ -0,0 +1,44 @@
+package com.test.firsttestfirebase.view.viewHolder;
+
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.test.firsttestfirebase.R;
+import com.test.firsttestfirebase.mapper.DataMapper;
+import com.test.firsttestfirebase.model.Relay;
+import com.test.firsttestfirebase.service.FirebaseService;
+
+public class RelayViewHolder extends RecyclerView.ViewHolder {
+    private TextView tvRelay;
+    private Switch swRelayAutomatic;
+    private Switch swRelayLight;
+
+    public RelayViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.tvRelay = itemView.findViewById(R.id.tv_relay);
+        this.swRelayAutomatic = itemView.findViewById(R.id.sw_relay_automatic);
+        this.swRelayLight = itemView.findViewById(R.id.sw_relay_light);
+    }
+
+    public void bind(Relay relay) {
+        this.tvRelay.setText(DataMapper.toCleanComponentAlias(relay.getAlias()));
+
+        this.swRelayAutomatic.setChecked(relay.isAutomatic());
+        this.setEnabledSwRelayLight();
+        this.swRelayAutomatic.setOnClickListener(view -> {
+            FirebaseService.getInstance().setRelayPropertyValue(relay.getNumber(), "automatic", swRelayAutomatic.isChecked());
+            this.setEnabledSwRelayLight();
+        });
+
+        this.swRelayLight.setChecked(DataMapper.toBoolean(relay.getRelayStatus()));
+        this.swRelayLight.setOnClickListener(view -> {
+            FirebaseService.getInstance().setRelayPropertyValue(relay.getNumber(), "status", DataMapper.toInteger(swRelayLight.isChecked()));
+        });
+    }
+
+    private void setEnabledSwRelayLight() {
+        this.swRelayLight.setEnabled(!this.swRelayAutomatic.isChecked());
+    }
+}
